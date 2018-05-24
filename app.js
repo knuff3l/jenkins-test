f2b0f47e696ed3e003a7f6e3ed2fa3cc9c20a381
tk <tobse_tobine@freenet.de>
2018-05-24 11:43:01 +0200

    etwas hübscher

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-    res.send("Hello Guys");
+    res.send('<h1 style="text-align: center;">Hello Guys</h1>');
 });
 
 app.listen(process.env.PORT || 4000);
